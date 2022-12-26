184e69660a98c5135be88bc804234a48f03e9994
Elliot Schlegelmilch <elliot@schlegelmilch.org>
2022-12-25 15:48:37 -1000

    split fact into freebsd version; update the path on the freebsd version.

--- lib/facter/wireguard_pubkeys.rb
+++ lib/facter/wireguard_pubkeys.rb
@@ -1,12 +1,35 @@
 # frozen_string_literal: true
 
 Facter.add(:wireguard_pubkeys) do
+  confine :kernel do |value|
+    value == "Linux"
+  end
+
   confine do
     File.directory?('/etc/wireguard/')
   end
   setcode do
     hash = {}
     Dir.glob('/etc/wireguard/*.pub').each do |file|
+      filename = file.split('/').last.gsub('.pub', '')
+      content = File.read(file)
+      hash[filename] = content.chomp
+    end
+    hash
+  end
+end
+
+Facter.add(:wireguard_pubkeys) do
+  confine :kernel do |value|
+    value == "FreeBSD"
+  end
+
+  confine do
+    File.directory?('/usr/local/etc/wireguard/')
+  end
+  setcode do
+    hash = {}
+    Dir.glob('/usr/local/etc/wireguard/*.pub').each do |file|
       filename = file.split('/').last.gsub('.pub', '')
       content = File.read(file)
       hash[filename] = content.chomp
